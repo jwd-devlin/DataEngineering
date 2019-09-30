f538781a504758e38ead8a970907cdc517fdaf90
Joshua Devlin <joshua.devlin@matmatch.com>
2019-09-30 17:33:34 +0200

    AirFlow access redshift

--- SongUdacity_Airflow/dags/udac_example_dag.py
+++ SongUdacity_Airflow/dags/udac_example_dag.py
@@ -5,6 +5,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
                                 LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
+import datatime
 
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
@@ -17,7 +18,10 @@ default_args = {
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          #start_date=datetime.datetime(2018, 1, 1, 0, 0, 0, 0),
+          #end_date=datetime.datetime(2018, 12, 1, 0, 0, 0, 0),
+          #schedule_interval="@monthly",
+          #max_active_runs=1
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
@@ -25,11 +29,21 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
     dag=dag
+    #table="trips",
+    #redshift_conn_id="redshift",
+    #aws_credentials_id="aws_credentials",
+    #s3_bucket="udac-data-pipelines",
+    #s3_key="divvy/partitioned/{execution_date.year}/{execution_date.month}/divvy_trips.csv"
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
     dag=dag
+    #table="trips",
+    #redshift_conn_id="redshift",
+    #aws_credentials_id="aws_credentials",
+    #s3_bucket="udac-data-pipelines",
+    #s3_key="divvy/partitioned/{execution_date.year}/{execution_date.month}/divvy_trips.csv"
 )
 
 load_songplays_table = LoadFactOperator(
@@ -63,3 +77,15 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
+start_operator >> [stage_events_to_redshift,stage_songs_to_redshift]
+
+[stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+
+
+load_songplays_table >> [load_user_dimension_table ,load_time_dimension_table, load_song_dimension_table,load_artist_dimension_table ]
+
+[load_user_dimension_table,  load_time_dimension_table, load_song_dimension_table, load_artist_dimension_table] >> run_quality_checks
+
+run_quality_checks >> end_operator
